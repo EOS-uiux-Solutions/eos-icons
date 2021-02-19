4984943236283adb9e24057571071f4fa9162334
Manuele Carlini <mcarlini@suse.com>
2021-02-19 14:21:05 +0000

    Fix admin icon adjust

--- dist/outlined/js/eos-icons.js
+++ dist/outlined/js/eos-icons.js
@@ -89,7 +89,9 @@ const eosIcons = [
     "category": "roles",
     "type": "static",
     "label": "EOS",
-    "date": "30/11/2020"
+    "date": "30/11/2020",
+    "hasOutlined": true,
+    "dateOutlined": "19/02/2021"
   },
   {
     "name": "ai",
